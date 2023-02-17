a2e40cc9b86f460e6dccb305b5b76d33c274d8c7
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2023-02-17 17:20:40 +0600

    deploy-url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ useEffect(()=>{
 const fetchPosts = async () =>{
   setLoading(true);
 try {
-    const response = await fetch('http://localhost:8080/api/v1/post', {
+    const response = await fetch('https://dall-e-8byo.onrender.com/api/v1/post', {
       method: 'GET', 
       headers: {
         'Content-Type': 'application/json',
